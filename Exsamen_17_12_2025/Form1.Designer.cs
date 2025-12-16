d71767a14f4403fa320c572f1d63172a72e85dfb
bogdi xameli <121572305+gmsugff@users.noreply.github.com>
2025-12-16 15:06:25 +0300

    Add files via upload

---
+++ Exsamen_17_12_2025/Form1.Designer.cs
@@ -0,0 +1,274 @@
+﻿namespace Exsamen_17_12_2025
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.radioButton1 = new System.Windows.Forms.RadioButton();
+            this.radioButton2 = new System.Windows.Forms.RadioButton();
+            this.radioButton3 = new System.Windows.Forms.RadioButton();
+            this.radioButton4 = new System.Windows.Forms.RadioButton();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.button6 = new System.Windows.Forms.Button();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+
+            // label1
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(157, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Слова для замены (каждое с новой строки):";
+
+            // textBox1 (слова для замены)
+            this.textBox1.Location = new System.Drawing.Point(12, 25);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.textBox1.Size = new System.Drawing.Size(200, 100);
+            this.textBox1.TabIndex = 1;
+
+            // label5 (новые слова)
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(12, 130);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(177, 13);
+            this.label5.TabIndex = 14;
+            this.label5.Text = "Новые слова (в том же порядке):";
+
+            // textBox3 (новые слова)
+            this.textBox3.Location = new System.Drawing.Point(12, 146);
+            this.textBox3.Multiline = true;
+            this.textBox3.Name = "textBox3";
+            this.textBox3.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.textBox3.Size = new System.Drawing.Size(200, 100);
+            this.textBox3.TabIndex = 15;
+
+            // button1 (Выбрать файл)
+            this.button1.Location = new System.Drawing.Point(12, 252);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(200, 30);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "Выбрать файл для обработки";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+
+            // button5 (Загрузить слова)
+            this.button5.Location = new System.Drawing.Point(12, 288);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(200, 30);
+            this.button5.TabIndex = 3;
+            this.button5.Text = "Загрузить пары слов из файла";
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+
+            // button6 (Пример)
+            this.button6.Location = new System.Drawing.Point(12, 324);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(200, 30);
+            this.button6.TabIndex = 16;
+            this.button6.Text = "Заполнить примером";
+            this.button6.UseVisualStyleBackColor = true;
+            this.button6.Click += new System.EventHandler(this.button6_Click);
+
+            // label2
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(218, 9);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(65, 13);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Результат:";
+
+            // textBox2 (результат)
+            this.textBox2.Location = new System.Drawing.Point(218, 25);
+            this.textBox2.Multiline = true;
+            this.textBox2.Name = "textBox2";
+            this.textBox2.ReadOnly = true;
+            this.textBox2.ScrollBars = System.Windows.Forms.ScrollBars.Both;
+            this.textBox2.Size = new System.Drawing.Size(400, 300);
+            this.textBox2.TabIndex = 5;
+
+            // groupBox1 (Режимы замены)
+            this.groupBox1.Controls.Add(this.radioButton4);
+            this.groupBox1.Controls.Add(this.radioButton3);
+            this.groupBox1.Controls.Add(this.radioButton2);
+            this.groupBox1.Controls.Add(this.radioButton1);
+            this.groupBox1.Location = new System.Drawing.Point(12, 360);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(200, 120);
+            this.groupBox1.TabIndex = 6;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Режим замены";
+
+            // radioButton1 (Обычная замена)
+            this.radioButton1.AutoSize = true;
+            this.radioButton1.Location = new System.Drawing.Point(6, 19);
+            this.radioButton1.Name = "radioButton1";
+            this.radioButton1.Size = new System.Drawing.Size(119, 17);
+            this.radioButton1.TabIndex = 0;
+            this.radioButton1.TabStop = true;
+            this.radioButton1.Text = "Обычная (полные слова)";
+            this.radioButton1.UseVisualStyleBackColor = true;
+
+            // radioButton2 (С учетом регистра)
+            this.radioButton2.AutoSize = true;
+            this.radioButton2.Location = new System.Drawing.Point(6, 42);
+            this.radioButton2.Name = "radioButton2";
+            this.radioButton2.Size = new System.Drawing.Size(126, 17);
+            this.radioButton2.TabIndex = 1;
+            this.radioButton2.Text = "С учетом регистра";
+            this.radioButton2.UseVisualStyleBackColor = true;
+
+            // radioButton3 (Любая часть)
+            this.radioButton3.AutoSize = true;
+            this.radioButton3.Location = new System.Drawing.Point(6, 65);
+            this.radioButton3.Name = "radioButton3";
+            this.radioButton3.Size = new System.Drawing.Size(130, 17);
+            this.radioButton3.TabIndex = 2;
+            this.radioButton3.Text = "Любая часть слова";
+            this.radioButton3.UseVisualStyleBackColor = true;
+
+            // radioButton4 (Удалить)
+            this.radioButton4.AutoSize = true;
+            this.radioButton4.Location = new System.Drawing.Point(6, 88);
+            this.radioButton4.Name = "radioButton4";
+            this.radioButton4.Size = new System.Drawing.Size(77, 17);
+            this.radioButton4.TabIndex = 3;
+            this.radioButton4.Text = "УДАЛИТЬ";
+            this.radioButton4.UseVisualStyleBackColor = true;
+
+            // button2 (Начать замену)
+            this.button2.Location = new System.Drawing.Point(12, 486);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(200, 40);
+            this.button2.TabIndex = 7;
+            this.button2.Text = "Начать замену";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+
+            // button3 (Очистить)
+            this.button3.Location = new System.Drawing.Point(218, 331);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(100, 30);
+            this.button3.TabIndex = 8;
+            this.button3.Text = "Очистить все";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+
+            // button4 (Сохранить результат)
+            this.button4.Location = new System.Drawing.Point(324, 331);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(140, 30);
+            this.button4.TabIndex = 9;
+            this.button4.Text = "Сохранить результат";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+
+            // label3 (Путь к файлу)
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(12, 529);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(37, 13);
+            this.label3.TabIndex = 10;
+            this.label3.Text = "Файл:";
+
+            // label4 (Статистика)
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(218, 529);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(85, 13);
+            this.label4.TabIndex = 11;
+            this.label4.Text = "Заменено слов: 0";
+
+            // Form1
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(634, 551);
+            this.Controls.Add(this.button6);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Замена слов в тексте";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.RadioButton radioButton1;
+        private System.Windows.Forms.RadioButton radioButton2;
+        private System.Windows.Forms.RadioButton radioButton3;
+        private System.Windows.Forms.RadioButton radioButton4;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.Button button6;
+    }
+}
+
+
---
+++ Exsamen_17_12_2025/Form1.cs
@@ -0,0 +1,362 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Reflection.Emit;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement.Button;
+
+namespace Exsamen_17_12_2025
+{
+        public partial class Form1 : Form
+        {
+            private string path;
+            private List<string> wordsToReplace = new List<string>();
+            private List<string> replacementWords = new List<string>();
+
+            public Form1()
+            {
+                InitializeComponent();
+            }
+
+            private void button2_Click(object sender, EventArgs e)
+            {
+                if (string.IsNullOrWhiteSpace(textBox1.Text))
+                {
+                    MessageBox.Show("Введите слова для замены!", "Ошибка",
+                        MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                    return;
+                }
+
+                if (string.IsNullOrWhiteSpace(textBox3.Text))
+                {
+                    MessageBox.Show("Введите слова для замены!", "Ошибка",
+                        MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                    return;
+                }
+
+                
+                wordsToReplace = textBox1.Text.Split('\n')
+                                  .Where(line => !string.IsNullOrWhiteSpace(line))
+                                  .Select(line => line.Trim())
+                                  .ToList();
+
+                replacementWords = textBox3.Text.Split('\n')
+                                  .Where(line => !string.IsNullOrWhiteSpace(line))
+                                  .Select(line => line.Trim())
+                                  .ToList();
+
+                
+                if (wordsToReplace.Count != replacementWords.Count)
+                {
+                    MessageBox.Show($"Количество слов не совпадает!\n" +
+                                  $"Слов для замены: {wordsToReplace.Count}\n" +
+                                  $"Слов для вставки: {replacementWords.Count}",
+                                  "Ошибка", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                    return;
+                }
+
+                if (wordsToReplace.Count == 0)
+                {
+                    MessageBox.Show("Не найдено слов для замены!", "Ошибка",
+                        MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                    return;
+                }
+
+                ProcessFile();
+            }
+
+            private void ProcessFile()
+            {
+                if (string.IsNullOrEmpty(path))
+                {
+                    MessageBox.Show("Сначала выберите файл!", "Ошибка",
+                        MessageBoxButtons.OK, MessageBoxIcon.Error);
+                    return;
+                }
+
+                try
+                {
+                    if (!File.Exists(path))
+                    {
+                        MessageBox.Show("Файл не найден!", "Ошибка",
+                            MessageBoxButtons.OK, MessageBoxIcon.Error);
+                        return;
+                    }
+
+                    
+                    string content = File.ReadAllText(path);
+                    string originalContent = content; 
+
+                    
+                    Dictionary<string, int> replacementStats = new Dictionary<string, int>();
+                    Dictionary<string, string> actualReplacements = new Dictionary<string, string>();
+
+                    
+                    for (int i = 0; i < wordsToReplace.Count; i++)
+                    {
+                        actualReplacements[wordsToReplace[i]] = replacementWords[i];
+                    }
+
+                    
+                    foreach (var replacement in actualReplacements)
+                    {
+                        string word = replacement.Key;
+                        string newWord = replacement.Value;
+
+                        if (string.IsNullOrWhiteSpace(word)) continue;
+
+                        
+                        string pattern;
+                        if (radioButton1.Checked) 
+                        {
+                            pattern = $@"\b{Regex.Escape(word)}\b";
+                        }
+                        else if (radioButton2.Checked) 
+                        {
+                            pattern = $@"\b{Regex.Escape(word)}\b";
+                            
+                            int count = Regex.Matches(content, pattern).Count;
+                            if (count > 0)
+                            {
+                                content = Regex.Replace(content, pattern, newWord,
+                                    RegexOptions.None, TimeSpan.FromSeconds(1));
+                                replacementStats[word] = count;
+                            }
+                            continue;
+                        }
+                        else if (radioButton3.Checked) 
+                        {
+                            pattern = Regex.Escape(word);
+                        }
+                        else if (radioButton4.Checked) 
+                        {
+                            pattern = $@"\b{Regex.Escape(word)}\b";
+                            newWord = "";
+                        }
+                        else
+                        {
+                            MessageBox.Show("Выберите режим замены!", "Ошибка",
+                                MessageBoxButtons.OK, MessageBoxIcon.Error);
+                            return;
+                        }
+
+                       
+                        int matchesCount = Regex.Matches(originalContent, pattern,
+                            radioButton2.Checked ? RegexOptions.None : RegexOptions.IgnoreCase).Count;
+
+                        if (matchesCount > 0)
+                        {
+                            content = Regex.Replace(content, pattern, newWord,
+                                radioButton2.Checked ? RegexOptions.None : RegexOptions.IgnoreCase,
+                                TimeSpan.FromSeconds(1));
+                            replacementStats[word] = matchesCount;
+                        }
+                    }
+
+                    
+                    textBox2.Text = content;
+
+                    
+                    string newPath = GetNewFilePath(path);
+                    File.WriteAllText(newPath, content);
+
+                   
+                    ShowStatistics(replacementStats, newPath);
+
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show($"Ошибка при обработке файла:\n{ex.Message}", "Ошибка",
+                        MessageBoxButtons.OK, MessageBoxIcon.Error);
+                }
+            }
+
+            private string GetNewFilePath(string originalPath)
+            {
+                string directory = Path.GetDirectoryName(originalPath);
+                string fileName = Path.GetFileNameWithoutExtension(originalPath);
+                string extension = Path.GetExtension(originalPath);
+
+                
+                string newFileName = $"{fileName}_modified{extension}";
+
+                return Path.Combine(directory, newFileName);
+            }
+
+            private void ShowStatistics(Dictionary<string, int> stats, string newPath)
+            {
+                if (stats.Count == 0)
+                {
+                    MessageBox.Show("Совпадений не найдено!", "Информация",
+                        MessageBoxButtons.OK, MessageBoxIcon.Information);
+                    return;
+                }
+
+                System.Text.StringBuilder sb = new System.Text.StringBuilder();
+                sb.AppendLine($"Обработка завершена!");
+                sb.AppendLine($"Режим: {(radioButton4.Checked ? "Удаление" : "Замена")}");
+                sb.AppendLine($"Новый файл: {Path.GetFileName(newPath)}");
+                sb.AppendLine($"\nСтатистика замен:");
+
+                int total = 0;
+                foreach (var stat in stats)
+                {
+                    int index = wordsToReplace.IndexOf(stat.Key);
+                    string replacement = index >= 0 && index < replacementWords.Count
+                        ? replacementWords[index]
+                        : "(удалено)";
+                    sb.AppendLine($"  '{stat.Key}' → '{replacement}': {stat.Value} замен");
+                    total += stat.Value;
+                }
+
+                sb.AppendLine($"\nВсего замен: {total}");
+
+                label4.Text = $"Заменено слов: {total}";
+                MessageBox.Show(sb.ToString(), "Результат",
+                    MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
+
+            private void button1_Click(object sender, EventArgs e)
+            {
+                using (OpenFileDialog openFileDialog = new OpenFileDialog())
+                {
+                    openFileDialog.Filter = "Текстовые файлы (*.txt)|*.txt|Все файлы (*.*)|*.*";
+                    openFileDialog.Title = "Выберите файл";
+                    openFileDialog.Multiselect = false;
+
+                    if (openFileDialog.ShowDialog() == DialogResult.OK)
+                    {
+                        path = openFileDialog.FileName;
+                        label3.Text = $"Файл: {Path.GetFileName(path)}";
+
+                        
+                        try
+                        {
+                            string preview = File.ReadAllText(path);
+                            textBox2.Text = preview.Length > 1000 ?
+                                preview.Substring(0, 1000) + "..." : preview;
+                        }
+                        catch (Exception ex)
+                        {
+                            MessageBox.Show($"Ошибка чтения файла:\n{ex.Message}", "Ошибка",
+                                MessageBoxButtons.OK, MessageBoxIcon.Error);
+                        }
+                    }
+                }
+            }
+
+            private void button3_Click(object sender, EventArgs e)
+            {
+               
+                textBox1.Clear();
+                textBox2.Clear();
+                textBox3.Clear();
+                wordsToReplace.Clear();
+                replacementWords.Clear();
+                label4.Text = "Заменено слов: 0";
+            }
+
+            private void button4_Click(object sender, EventArgs e)
+            {
+               
+                if (string.IsNullOrWhiteSpace(textBox2.Text))
+                {
+                    MessageBox.Show("Нет данных для сохранения!", "Ошибка",
+                        MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                    return;
+                }
+
+                using (SaveFileDialog saveFileDialog = new SaveFileDialog())
+                {
+                    saveFileDialog.Filter = "Текстовые файлы (*.txt)|*.txt|Все файлы (*.*)|*.*";
+                    saveFileDialog.Title = "Сохранить результат";
+                    saveFileDialog.FileName = "результат.txt";
+
+                    if (saveFileDialog.ShowDialog() == DialogResult.OK)
+                    {
+                        try
+                        {
+                            File.WriteAllText(saveFileDialog.FileName, textBox2.Text);
+                            MessageBox.Show("Файл успешно сохранен!", "Успех",
+                                MessageBoxButtons.OK, MessageBoxIcon.Information);
+                        }
+                        catch (Exception ex)
+                        {
+                            MessageBox.Show($"Ошибка сохранения:\n{ex.Message}", "Ошибка",
+                                MessageBoxButtons.OK, MessageBoxIcon.Error);
+                        }
+                    }
+                }
+            }
+
+            private void button5_Click(object sender, EventArgs e)
+            {
+                
+                using (OpenFileDialog openFileDialog = new OpenFileDialog())
+                {
+                    openFileDialog.Filter = "Текстовые файлы (*.txt)|*.txt|Все файлы (*.*)|*.*";
+                    openFileDialog.Title = "Загрузить пары слов (старое=новое)";
+
+                    if (openFileDialog.ShowDialog() == DialogResult.OK)
+                    {
+                        try
+                        {
+                            string[] lines = File.ReadAllLines(openFileDialog.FileName)
+                                               .Where(line => !string.IsNullOrWhiteSpace(line) &&
+                                                              !line.StartsWith("#") &&
+                                                              line.Contains('='))
+                                               .ToArray();
+
+                            List<string> oldWords = new List<string>();
+                            List<string> newWords = new List<string>();
+
+                            foreach (string line in lines)
+                            {
+                                string[] parts = line.Split('=');
+                                if (parts.Length >= 2)
+                                {
+                                    oldWords.Add(parts[0].Trim());
+                                    newWords.Add(parts[1].Trim());
+                                }
+                            }
+
+                            textBox1.Text = string.Join("\n", oldWords);
+                            textBox3.Text = string.Join("\n", newWords);
+
+                            MessageBox.Show($"Загружено {oldWords.Count} пар слов", "Информация",
+                                MessageBoxButtons.OK, MessageBoxIcon.Information);
+                        }
+                        catch (Exception ex)
+                        {
+                            MessageBox.Show($"Ошибка загрузки:\n{ex.Message}", "Ошибка",
+                                MessageBoxButtons.OK, MessageBoxIcon.Error);
+                        }
+                    }
+                }
+            }
+
+            private void Form1_Load(object sender, EventArgs e)
+            {
+                
+                radioButton1.Checked = true; 
+            }
+
+            private void button6_Click(object sender, EventArgs e)
+            {
+                
+                textBox1.Text = "плохой\nстарый\nмедленный\nошибка";
+                textBox3.Text = "хороший\nновый\nбыстрый\nисправлено";
+            }
+
+    }
+} 
+
+
